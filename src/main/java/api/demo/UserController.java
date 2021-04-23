ee14dade2b0c72caf3270174d9aa3b35bd935752
Chris Geer <cgeer3@gmail.com>
2021-04-23 10:48:57 -0400

    Added support for delegated API calls (GraphAPI)

--- src/main/java/api/demo/UserController.java
+++ src/main/java/api/demo/UserController.java
@@ -1,12 +1,78 @@
 package api.demo;
 
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private static final String GRAPH_ME_ENDPOINT = "https://graph.microsoft.com/v1.0/me";
+
+    @Autowired
+    private WebClient webClient;
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
+    /**
+     * Call the graph resource, return user information
+     * @return Response with graph data
+     */
+    @GetMapping("call-graph-with-repository")
+    public String callGraphWithRepository() {
+        Authentication principal = SecurityContextHolder.getContext().getAuthentication();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) requestAttributes;
+        OAuth2AuthorizedClient graph = oAuth2AuthorizedClientRepository
+            .loadAuthorizedClient("graph", principal, sra.getRequest());
+        return callMicrosoftGraphMeEndpoint(graph);
+    }
+
+    /**
+     * Call the graph resource with annotation, return user information
+     * @param graph authorized client for Graph
+     * @return Response with graph data
+     */
+    @GetMapping("call-graph")
+    public String callGraph(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graph) {
+        return callMicrosoftGraphMeEndpoint(graph);
+    }
+
+    /**
+     * Call microsoft graph me endpoint
+     * @param graph Authorized Client
+     * @return Response string data.
+     */
+    private String callMicrosoftGraphMeEndpoint(OAuth2AuthorizedClient graph) {
+        if (null != graph) {
+            String body = webClient
+                .get()
+                .uri(GRAPH_ME_ENDPOINT)
+                .attributes(oauth2AuthorizedClient(graph))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+            System.out.println("Response from Graph: " + body);
+            return "Graph response " + (null != body ? "success." : "failed.");
+        } else {
+            return "Graph response failed.";
+        }
+    }
 
     @GetMapping("/status/check")
     public String status() {
